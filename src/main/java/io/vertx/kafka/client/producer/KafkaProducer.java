e07b110e5c59b4f631e84d38c14648453c3a2622
ppatierno <ppatierno@live.com>
2016-12-19 18:43:09 +0100

    Fixed create methods without @GenIgnore (due to Properties) Fixed create method with Map<String, Object> not allowed

--- src/main/java/io/vertx/kafka/client/producer/KafkaProducer.java
+++ src/main/java/io/vertx/kafka/client/producer/KafkaProducer.java
@@ -1,12 +1,14 @@
 package io.vertx.kafka.client.producer;
 
 import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.streams.WriteStream;
 import io.vertx.kafka.client.producer.impl.KafkaProducerImpl;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -16,22 +18,24 @@ import java.util.Properties;
 @VertxGen
 public interface KafkaProducer<K, V> extends WriteStream<KafkaProducerRecord<K, V>> {
 
+  static <K, V> KafkaProducer<K, V> create(Vertx vertx, Map<String, String> config) {
+    KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, new HashMap<>(config));
+    return new KafkaProducerImpl<>(stream);
+  }
+
+  static <K, V> KafkaProducer<K, V> create(Vertx vertx, Map<String, String> config, Class<K> keyType, Class<V> valueType) {
+    KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, new HashMap<>(config), keyType, valueType);
+    return new KafkaProducerImpl<>(stream);
+  }
+
+  @GenIgnore
   static <K, V> KafkaProducer<K, V> create(Vertx vertx, Properties config) {
     KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, config);
     return new KafkaProducerImpl<>(stream);
   }
 
+  @GenIgnore
   static <K, V> KafkaProducer<K, V> create(Vertx vertx, Properties config, Class<K> keyType, Class<V> valueType) {
-    KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, config, keyType, valueType);
-    return new KafkaProducerImpl<>(stream);
-  }
-
-  static <K, V> KafkaProducer<K, V> create(Vertx vertx, Map<String, Object> config) {
-    KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, config);
-    return new KafkaProducerImpl<>(stream);
-  }
-
-  static <K, V> KafkaProducer<K, V> create(Vertx vertx, Map<String, Object> config, Class<K> keyType, Class<V> valueType) {
     KafkaWriteStream<K, V> stream = KafkaWriteStream.create(vertx, config, keyType, valueType);
     return new KafkaProducerImpl<>(stream);
   }
