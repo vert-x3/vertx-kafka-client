ba5acf47045d1a4bec845464b740b8cec4a1a322
Julien Viet <julien@julienviet.com>
2017-10-11 15:19:41 +0200

    Minor

--- src/main/java/io/vertx/kafka/client/consumer/impl/KafkaConsumerImpl.java
+++ src/main/java/io/vertx/kafka/client/consumer/impl/KafkaConsumerImpl.java
@@ -339,7 +339,7 @@ public class KafkaConsumerImpl<K, V> implements KafkaConsumer<K, V> {
 
   @Override
   public void commit(Handler<AsyncResult<Void>> completionHandler) {
-    this.stream.commit(completionHandler != null ? ar -> ar.map((Object) null) : null);
+    this.stream.commit(completionHandler != null ? AsyncResult::mapEmpty : null);
   }
 
   @Override
