5f316723cd7f297ba0940879a59b0728b65ec063
Paul Gallagher <pgallagh@redhat.com>
2019-07-11 16:08:13 +0100

    missing vertx core Future import

--- src/test/java/io/vertx/kafka/client/tests/CleanupTest.java
+++ src/test/java/io/vertx/kafka/client/tests/CleanupTest.java
@@ -19,6 +19,7 @@ package io.vertx.kafka.client.tests;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
