0a495e08ef9a8ef83cafba5a2e7932371fdde145
Julien Viet <julien@julienviet.com>
2024-10-07 23:42:27 +0200

    Regen converters

--- src/main/generated/io/vertx/kafka/client/common/KafkaClientOptionsConverter.java
+++ src/main/generated/io/vertx/kafka/client/common/KafkaClientOptionsConverter.java
@@ -4,16 +4,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.kafka.client.common.KafkaClientOptions}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.kafka.client.common.KafkaClientOptions} original class using Vert.x codegen.
  */
 public class KafkaClientOptionsConverter {
-
-  private static final Base64.Decoder BASE64_DECODER = Base64.getUrlDecoder();
-  private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, KafkaClientOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
