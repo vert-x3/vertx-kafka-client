39a5a8410b8aaa08481376b9bddc75c014ba9495
Kartikey-Mishra1 <kartikeymishra.211199@gmail.com>
2025-01-20 15:18:32 +0530

    fix: Fetch and initialize additional ConfigEntry properties
    Signed-off-by: Kartikey-Mishra1 <kartikeymishra.211199@gmail.com>

--- src/main/java/io/vertx/kafka/admin/ConfigEntry.java
+++ src/main/java/io/vertx/kafka/admin/ConfigEntry.java
@@ -57,6 +57,26 @@ public class ConfigEntry {
   }
 
   /**
+   * Constructor
+   * 
+   * @param name the non-null config name  
+   * @param value the config value or null
+   * @param source the source of this config entry 
+   * @param isSensitive whether the config value is sensitive, the broker never returns the value if it is sensitive
+   * @param isReadOnly whether the config is read-only and cannot be updated
+   * @param synonyms Synonym configs in order of precedence
+   */
+  public ConfigEntry(String name, String value, ConfigSource source, boolean isSensitive, 
+                    boolean isReadOnly, List<ConfigSynonym> synonyms) {
+    this.name = name;
+    this.value = value;
+    this.source = source;
+    this.isSensitive = isSensitive;
+    this.isReadOnly = isReadOnly;
+    this.synonyms = synonyms;
+  }
+
+  /**
    * Constructor (from JSON representation)
    *
    * @param json  JSON representation
